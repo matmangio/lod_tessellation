dd330d63355f4b8338fedbbea2be50c63e03a76d
Matteo Mangioni <mat.mangioni@gmail.com>
2025-08-22 18:20:53 +0200

    Add libraries and basic project structure
    Signed-off-by: Matteo Mangioni <mat.mangioni@gmail.com>

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <glfw/glfw3.h>
+
+// CONSTANTS
+const int screen_dimensions[2] = {1200, 900};
+
+using namespace std;
+
+int main() {
+    cout << "Hello World!";
+}
